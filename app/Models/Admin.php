4a449a75a73712cfb0e395116324ee64acd01aab
fahadsolihin04 <fahadsolihin04@gmail.com>
2024-09-20 01:49:54 +0700

    1.Installasi laravel 11 brezze with alpine blade, php unit. 2.Laravel 11 Multi Guard Authentication, Multiple authentication for User & Admin in Laravel(Admin and User can register, login, and logout) note:PR= membuat navbar/sidebar admin <supaya terpisah dengan navbar user/supaya punya navigasi masing-masing dari view admin dan user>

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,16 +2,16 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class User extends Authenticatable
+class Admin extends Authenticatable
 {
     use HasFactory, Notifiable;
 
-
+    protected $guard= 'admin';
     /**
      * The attributes that are mass assignable.
      *
